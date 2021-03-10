20e763dd7a96f23a937b9e87521535423aebf185
Polina Kravchenko <lotushiems@gmail.com>
2021-03-10 11:24:49 +0300

    add source file

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static void railFence() {
+        String text;
+        int key;
+        Scanner in = new Scanner(System.in);
+        String answer;
+        boolean rightInput = false;
+
+        System.out.print("    Введите сообщение: ");
+        text = in.nextLine();
+        System.out.print("    Введите ключ: ");
+        key = Integer.parseInt(in.nextLine());
+
+        while (!rightInput) {
+            System.out.print("\n    Вы хотите зашифровать сообщение? Если ответ да, то введите YES, иначе же NO.\n");
+            answer = "";
+            while (answer.isEmpty()) {
+                answer = in.nextLine();
+            }
+            rightInput = true;
+            if (answer.equals("YES")) {
+                System.out.printf("\n    Зашифрованное сообщение: %s\n", RailFence.encryptRailFence(text, key));
+            } else if (answer.equals("NO")) {
+                System.out.printf("\n    Расшифрованное сообщение: %s\n", RailFence.decryptRailFence(text, key));
+            }
+            else {
+                System.out.printf("\n    Вы неправильно ответили.\n");
+                rightInput = false;
+            }
+        }
+    }
+
+    private static void menu() {
+        boolean rightInput = false;
+        Scanner in = new Scanner(System.in);
+        int method;
+        boolean repeat = true;
+
+        while (repeat) {
+            String trash = in.nextLine();
+
+            rightInput = false;
+
+            System.out.printf("\n        Вам предложен выбор метода: \n");
+            System.out.println("    1. Метод железнодорожной изгороди.");
+            System.out.println("    2. Столбцовый метод.");
+            System.out.println("    3. Метод поворащивающейся решетки.");
+            System.out.println("    4. Шифр Вижинера.");
+            System.out.println("    5. Выход.");
+
+            while (!rightInput) {
+                System.out.printf("\n    Напишите номер метода, который хотите использовать: ");
+                method = Integer.parseInt(in.nextLine());
+                rightInput = true;
+                switch (method) {
+                    case 1:
+                        railFence();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        repeat = false;
+                        break;
+                    default:
+                        rightInput = false;
+                        System.out.printf("\n    Вы неправильно ввели номер метода.\n");
+                }
+            }
+        }
+    }
+
+    public static void main(String Args[]) {
+        menu();
+    }
+}
---
+++ src/RailFence.java
@@ -0,0 +1,102 @@
+public class RailFence {
+
+    public static String encryptRailFence(String text, int key) {
+        char[][] rail = new char[key][text.length()];
+
+        for (int i = 0; i < key; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                rail[i][j] = '\n';
+            }
+        }
+
+        boolean dirDown = false;
+        int row = 0;
+        int column = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (row == 0 || row == key - 1) {
+                dirDown = !dirDown;
+            }
+
+            rail[row][column++] = text.charAt(i);
+
+            if (dirDown) {
+                row++;
+            } else {
+                row--;
+            }
+        }
+
+        String result = "";
+
+        for (int i = 0; i < key; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (rail[i][j] != '\n') {
+                    result += rail[i][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static String decryptRailFence(String text, int key) {
+        char[][] rail = new char[key][text.length()];
+
+        for (int i = 0; i < key; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                rail[i][j] = '\n';
+            }
+        }
+
+        boolean dirDown = false;
+        int row = 0;
+        int column = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (row == 0 || row == key - 1) {
+                dirDown = !dirDown;
+            }
+
+            rail[row][column++] = '%';
+
+            if (dirDown) {
+                row++;
+            } else {
+                row--;
+            }
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < key; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (rail[i][j] == '%') {
+                    rail[i][j] = text.charAt(index++);
+                }
+            }
+        }
+
+        String result = "";
+
+        dirDown = false;
+        row = 0;
+        column = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (row == 0 || row == key - 1) {
+                dirDown = !dirDown;
+            }
+
+            result += rail[row][column++];
+
+            if (dirDown) {
+                row++;
+            } else {
+                row--;
+            }
+        }
+
+        return result;
+    }
+}
